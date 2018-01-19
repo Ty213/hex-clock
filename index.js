8a62dab40de2c1b39214a91acc275172440f8f2f
Ty Thompson <tylermoko@gmail.com>
2018-01-19 11:55:05 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// For this assignment you will be creating a digital clock using HTML, CSS and JavaScript.
+//  The finished product will be fully functional, styled and ready to show off to friends!
+
+// Note: You will choose one of the two different options below to complete this assignment.
+// Details for each are below.
+//  Remember whichever you don’t do now you can return to later as a fun side project!
+
+// Hint: This assignment is to gain the skills needed for development, do NOT submit copied code.
+
+// Option 1:
+
+// The hex clock is a hex code that reflects the time of day in #hhmmss format instead of hh:mm:ss format.
+//  The background color of the screen is then changed based on the hex code which is created by the current time.
+
+// Option 1:
+
+// JS clock is fully functional and throws zero errors
+// The time is in digital format to resemble a hex code
+// The time is displayed centered in the middle of the screen
+// The screen background color changes based on the current time
